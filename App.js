6b0451adba27f56597305b0516ef36e8b51d0245
SangWon7242 <gotkdqja@gmail.com>
2024-10-13 14:12:14 +0900

    Dimensions 반영하여 모바일 해상도 구하고, 한 화면에 날짜, 온도가 표시되도록 UI 변경

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button, ScrollView } from "react-native";
+import { Dimensions } from "react-native";
 import React, { useState } from "react";
+
+// const { width: SCREEN_WIDTH } = Dimensions.get("window");
+
+const SCREEN_WIDTH = Dimensions.get("window").width;
 
 const App = () => {
   return (
@@ -8,37 +13,41 @@ const App = () => {
       <View style={styles.cityCon}>
         <Text style={styles.city}>Ansan</Text>
       </View>
-      <ScrollView horizontal contentContainerStyle={styles.weather}>
+      <View style={styles.regDateCon}>
+        <Text style={styles.regDate}>10월 13일, 일, 13:18</Text>
+      </View>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
         <View style={styles.weatherInner}>
           <View style={styles.day}>
-            <Text style={styles.regDate}>10월 13일, 일, 13:18</Text>
             <Text style={styles.desc}>맑음</Text>
           </View>
           <View style={styles.tempCon}>
             <Text style={styles.temp}>24</Text>
           </View>
         </View>
-        <View style={styles.weatherCon}>
+        <View style={styles.weatherInner}>
           <View style={styles.day}>
-            <Text style={styles.regDate}>10월 13일, 일, 13:18</Text>
             <Text style={styles.desc}>맑음</Text>
           </View>
           <View style={styles.tempCon}>
             <Text style={styles.temp}>24</Text>
           </View>
         </View>
-        <View style={styles.weatherCon}>
+        <View style={styles.weatherInner}>
           <View style={styles.day}>
-            <Text style={styles.regDate}>10월 13일, 일, 13:18</Text>
             <Text style={styles.desc}>맑음</Text>
           </View>
           <View style={styles.tempCon}>
             <Text style={styles.temp}>24</Text>
           </View>
         </View>
-        <View style={styles.weatherCon}>
+        <View style={styles.weatherInner}>
           <View style={styles.day}>
-            <Text style={styles.regDate}>10월 13일, 일, 13:18</Text>
             <Text style={styles.desc}>맑음</Text>
           </View>
           <View style={styles.tempCon}>
@@ -57,7 +66,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#ffe01a",
   },
   cityCon: {
-    flex: 1,
+    flex: 0.3,
   },
   city: {
     flex: 1,
@@ -67,16 +76,8 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontWeight: "bold",
   },
-  weather: {
-    backgroundColor: "blue",
-  },
-  weatherInner: {
-    flex: 3,
-  },
-  day: {
-    flex: 0.2,
+  regDateCon: {
     alignItems: "center",
-    justifyContent: "center",
   },
   regDate: {
     paddingTop: 10,
@@ -88,6 +89,16 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     borderRadius: 20,
     overflow: "hidden",
+  },
+  weather: {},
+  weatherInner: {
+    flex: 3,
+    width: SCREEN_WIDTH,
+  },
+  day: {
+    flex: 0.2,
+    alignItems: "center",
+    justifyContent: "center",
   },
   desc: {
     flex: 1.5,
